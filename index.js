93274c8f6558df53b70ffe5229280e9a8444a87d
Saad Farooq <saad_farooq96@hotmail.com>
2020-07-31 18:27:20 +0500

    First Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let stopMsg = [
+    'NO! NO MOVING THE MOUSE.',
+    'STOP IT!',
+    'IT TICKLES'
+];
+let mouseMoving = false;
+
+$(document).ready(
+    () => {
+        let msg = document.querySelector('#msg');
+        let timeout;
+        document.querySelector('#panel').addEventListener('mousemove', () => {
+            let n = Math.floor(Math.random() * 1000) % stopMsg.length;
+            if(!mouseMoving){
+                msg.innerHTML = stopMsg[n];
+                mouseMoving = true;
+            }
+            if (timeout !== undefined) {
+                window.clearTimeout(timeout);
+            }
+            timeout = window.setTimeout(function () {
+                mouseMoving = false;
+                msg.innerHTML = 'OKAY, THATS BETTER.';
+            }, 500);
+        });
+    }
+);
